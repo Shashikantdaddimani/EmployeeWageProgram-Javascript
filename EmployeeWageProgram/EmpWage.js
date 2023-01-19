0554d0dfc5a5fcfb2c3b82fedb16be95ee514f41
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-19 10:35:21 +0530

    Added:Logic for Calculating the EmployeeWage Using Switch case

--- EmployeeWageProgram/EmpWage.js
+++ EmployeeWageProgram/EmpWage.js
@@ -1,9 +1,20 @@
-const Is_Absent=0
+const Is_Part_Time=1;
+const Is_Full_Time=2;
+const Part_Time_Hours=4;
+const Full_Time_Hours=8;
+const Wage_Per_Hour=20;
+let empHrs=0;
+empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case Is_Part_Time:
+        empHrs = Part_Time_Hours;
+        break;
+    case Is_Full_Time:
+        empHrs = Full_Time_Hours;
+        break;
+    default:
+        empHrs=0;
 
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==Is_Absent){
-    console.log("Employee is Absent");
-
-}else{
-    console.log("Employee is Present");
 }
+let empWage = empHrs* Wage_Per_Hour;
+console.log("Emp wage: "+empWage);
