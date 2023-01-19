59282cc372f2933955dab6e7ba229001dd8a3c6b
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-19 10:41:45 +0530

    Added:Logic for checking Employee Working hours using with Function

--- EmployeeWageProgram/EmpWage.js
+++ EmployeeWageProgram/EmpWage.js
@@ -1,20 +1,26 @@
+
+/*
+ getWorkingHours is Method for Checking Employee Working hours
+ */
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case Is_Part_Time:
+            return Part_Time_Hours;
+        case Is_Full_Time:
+            return Full_Time_Hours;
+        default:
+            return 0;
+
+    }
+}
 const Is_Part_Time=1;
 const Is_Full_Time=2;
 const Part_Time_Hours=4;
 const Full_Time_Hours=8;
 const Wage_Per_Hour=20;
-let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case Is_Part_Time:
-        empHrs = Part_Time_Hours;
-        break;
-    case Is_Full_Time:
-        empHrs = Full_Time_Hours;
-        break;
-    default:
-        empHrs=0;
 
-}
-let empWage = empHrs* Wage_Per_Hour;
-console.log("Emp wage: "+empWage);
+let empHrs=0;
+let empCheck =Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
+let empWage =empHrs* Wage_Per_Hour;
+console.log("Emp Wage: "+empWage);
